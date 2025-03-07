ea51c10914ccc56d8eccb3b270f71b2054fb1bb3
Olivier Biot <olivier.biot@gmail.com>
2025-03-07 12:07:58 +0100

    Refactor GraphNameProvider to simplify name retrieval logic for elements and styles.

--- src/lsp/graph-name-provider.ts
+++ src/lsp/graph-name-provider.ts
@@ -2,21 +2,19 @@ import { AstNode, DefaultNameProvider, isNamed } from 'langium';
 
 import { isElement, isStyle } from '../language/generated/ast.js';
 
-// Adjust the import path
-
 export class GraphNameProvider extends DefaultNameProvider {
   override getName(node: AstNode): string | undefined {
-    if (isElement(node) && isNamed(node) && node.name.length > 0) {
-      // Link can have undefined 'name'
-      //console.log(`GraphNameProvider.getName(type: ${node.$type}) - name = "${node.name ?? ''}"`);
-      return node.name;
-    }
-    if (isStyle(node) && node.name.length > 0) {
-      //console.log(`GraphNameProvider.getName(type: ${node.$type}) - name = "${node.name}"`);
-      return node.name;
+    if (isNamed(node) && node.name.length > 0) {
+      // Non-empty name
+      if (isElement(node)) {
+        return node.name;
+      }
+      if (isStyle(node)) {
+        return node.name;
+      }
     }
 
-    //console.warn(`GraphNameProvider.getName(type: ${node.$type}) - WILL NEVE RETURN A NAME`);
+    //console.warn(`GraphNameProvider.getName(type: ${node.$type}) - no name or empty name found`);
     return undefined;
   }
 }
